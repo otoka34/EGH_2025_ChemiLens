dd75bf778e37ae3ac470b22ce3979af8e8413dfb
otoka34 <118039939+otoka34@users.noreply.github.com>
2025-09-18 20:59:11 +0900

    feat: ホーム画面等修正

--- lib/screens/collection/history_screen.dart
+++ lib/screens/collection/history_screen.dart
@@ -20,7 +20,7 @@ class HistoryScreen extends ConsumerStatefulWidget {
 class _HistoryScreenState extends ConsumerState<HistoryScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
-
+  bool _isLoading = false;
   @override
   void initState() {
     super.initState();
